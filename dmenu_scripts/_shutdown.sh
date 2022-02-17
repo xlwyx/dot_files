409fae24dce2387d322cefb64894b8c3306a237b
xlwyx <83835896+xlwyx@users.noreply.github.com>
2022-02-17 09:07:09 -0500

    Update and rename openrc_shutdown.sh to _shutdown.sh

--- dmenu_scripts/_shutdown.sh
+++ dmenu_scripts/_shutdown.sh
@@ -3,9 +3,9 @@
 r=$(echo -e "reboot\nshutdown\nshuttimer\ncancel" | dmenu -i -fn "Awesome-20" -nb "#455" -nf "#ff9860" -sb "#f56" -sf "#03f")
 
 case "$r" in
-        reboot) doas openrc-shutdown -r now  ;;
-        shutdown) doas openrc-shutdown -h now  ;;
-        shuttimer) doas openrc-shutdown -t 8:20  ;;
+        reboot) doas shutdown -r now  ;;
+        shutdown) doas shutdown -h now  ;;
+        shuttimer) doas shutdown -t 8:20  ;;
         cancel) doas openrc-shutdown -c  ;;
         *) echo "No option has been selected" ;;
 esac
