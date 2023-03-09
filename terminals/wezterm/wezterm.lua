d1958a505bf7080c3e2ee3aac4f19d57cca8499d
xlwyx <83835896+xlwyx@users.noreply.github.com>
2023-03-09 17:12:43 -0500

    Update wezterm.lua

--- terminals/wezterm/wezterm.lua
+++ terminals/wezterm/wezterm.lua
@@ -3,43 +3,43 @@ local wezterm = require('wezterm')
 return {
 
   colors = {
-      -- The default text color
-      foreground = 'silver',
-      -- The default background color
-      background = 'white',
+    -- The default text color
+    foreground = '#afe7b6',
+    -- The default background color
+    background = '#280053',
 
-      -- Overrides the cell background color when the current cell is occupied by the
-      -- cursor and the cursor style is set to Block
-      cursor_bg = '#04395e',
-      -- Overrides the text color when the current cell is occupied by the cursor
-      cursor_fg = 'black',
-      -- Specifies the border color of the cursor when the cursor style is set to Block,
-      -- or the color of the vertical or horizontal bar when the cursor style is set to
-      -- Bar or Underline.
-      cursor_border = '#fcf300',
+    -- Overrides the cell background color when the current cell is occupied by the
+    -- cursor and the cursor style is set to Block
+    cursor_bg = '#04395e',
+    -- Overrides the text color when the current cell is occupied by the cursor
+    cursor_fg = 'black',
+    -- Specifies the border color of the cursor when the cursor style is set to Block,
+    -- or the color of the vertical or horizontal bar when the cursor style is set to
+    -- Bar or Underline.
+    cursor_border = '#fcf300',
 
-      -- the foreground color of selected text
-      selection_fg = '#1f7a8c',
-      -- the background color of selected text
-      selection_bg = '#aeb8fe',
+    -- the foreground color of selected text
+    selection_fg = '#1f7a8c',
+    -- the background color of selected text
+    selection_bg = '#aeb8fe',
 
-      -- The color of the scrollbar "thumb"; the portion that represents the current viewport
-      scrollbar_thumb = '#222222',
+    -- The color of the scrollbar "thumb"; the portion that represents the current viewport
+    scrollbar_thumb = '#222222',
 
-      -- The color of the split lines between panes
-      split = '#444444',
+    -- The color of the split lines between panes
+    split = '#444444',
 
-      ansi = {'black', 'maroon', 'green', 'olive', 'navy', 'purple', 'teal', 'silver'},
-      brights = {'grey', 'red', 'lime', 'yellow', 'blue', 'fuchsia', 'aqua', 'white'},
+    ansi = {'black', 'maroon', 'green', 'olive', 'navy', 'purple', 'teal', 'silver'},
+    brights = {'grey', 'red', 'lime', 'yellow', 'blue', 'fuchsia', 'aqua', 'white'},
 
-      -- Arbitrary colors of the palette in the range from 16 to 255
-      indexed = {[136] = '#af8700'},
+    -- Arbitrary colors of the palette in the range from 16 to 255
+    indexed = {[136] = '#af8700'},
 
-      -- Since: 20220319-142410-0fcdea07
-      -- When the IME, a dead key or a leader key are being processed and are effectively
-      -- holding input pending the result of input composition, change the cursor
-      -- to this color to give a visual cue about the compose state.
-      compose_cursor = 'fuchsia',
+    -- Since: 20220319-142410-0fcdea07
+    -- When the IME, a dead key or a leader key are being processed and are effectively
+    -- holding input pending the result of input composition, change the cursor
+    -- to this color to give a visual cue about the compose state.
+    compose_cursor = 'fuchsia',
   },
 
   window_frame = {
@@ -57,18 +57,16 @@ return {
 
     -- The overall background color of the tab bar when
     -- the window is focused
-    active_titlebar_bg = '#00044c',
+    active_titlebar_bg = '#b8918c',
 
     -- The overall background color of the tab bar when
     -- the window is not focused
-    inactive_titlebar_bg = '#00022d',
+    inactive_titlebar_bg = '#ff6666',
   },
 
-  colors = {
-    tab_bar = {
-      -- The color of the inactive tab bar edge/divider
-      inactive_tab_edge = '#00011e',
-    },
+  tab_bar = {
+  -- The color of the inactive tab bar edge/divider
+    inactive_tab_edge = '#00011e',
   },
 
   -- You can specify some parameters to influence the font selection;
@@ -87,9 +85,9 @@ return {
 
   -- This increases color saturation by 50%
   foreground_text_hsb = {
-    hue = 0.4,
-    saturation = 0.6,
-    brightness = 0.5,
+    hue = 0.6,
+    saturation = 0.7,
+    brightness = 0.6,
   },
 
   hide_tab_bar_if_only_one_tab = true,
@@ -101,12 +99,12 @@ return {
   font_dirs = {'Daddy Time Mono'},
 
   -- Launch a command on Working Directory	
-  label = 'List files in /home/bitx/ ',
+  label = 'List files in /home/lwyx/ ',
   args = {'ls", "-al'},
-  cwd = '/home/bitx/ ',
+  cwd = '/home/lwyx/ ',
 
   window_backbground_opacity = 0.9,
-  text_background_opacity = 0.2,
+  text_background_opacity = 0.3,
 
   -- How many lines of scrollback you want to retain per tab
   scrollback_lines = 3500,
